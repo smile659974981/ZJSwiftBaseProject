7213ec450099ece5fc9f4e107dd02dd2f935979a
silence <silence@silence.local>
2020-05-12 14:43:54 +0800

    tabbar

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
+platform :ios, '13.0'
+use_frameworks!
+
+target 'ZJSwiftBaseProject' do
+  pod 'RAMAnimatedTabBarController'
+end
---
+++ ZJSwiftBaseProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ZJSwiftBaseProject
+//
+//  Created by silence on 2020/5/12.
+//  Copyright © 2020 silence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+}
+
